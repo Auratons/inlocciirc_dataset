8aa95a63f51e5d91ebcbc62e496c11ed77d367ee
Tomas Kremel <kremelt@gmail.com>
2022-10-06 00:02:14 +0200

    Checking of output file existence in file listing added.

--- inloc_build_file_lists/inloc_build_file_lists.m
+++ inloc_build_file_lists/inloc_build_file_lists.m
@@ -15,18 +15,22 @@ function inloc_build_file_lists(varargin)
     types = {'db', 'query'};
     for idx = 1:length(types)
         type = types{idx};
-
-        glob = get_with_default(params, sprintf('input_%s_glob', type), '*.jpg');
-        files = dir(fullfile(params.(sprintf('input_%s_dir', type)), glob));
-        nFiles = size(files, 1);
-        filenames = cell(1, nFiles);
-
-        for i=1:nFiles
-            filenames{1, i} = fullfile(files(i).folder, files(i).name);
-        end
-
         output_mat_path = params.(sprintf('output_%s_mat_path', type));
-        create_parent_folder(output_mat_path);
-        save(output_mat_path, 'filenames');
+
+        if exist(output_mat_path, 'file') ~= 2
+            glob = get_with_default(params, sprintf('input_%s_glob', type), '*.jpg');
+            files = dir(fullfile(params.(sprintf('input_%s_dir', type)), glob));
+            nFiles = size(files, 1);
+            filenames = cell(1, nFiles);
+
+            for i=1:nFiles
+                filenames{1, i} = fullfile(files(i).folder, files(i).name);
+            end
+
+            create_parent_folder(output_mat_path);
+            save(output_mat_path, 'filenames');
+        else
+            fprintf('SKIPPING BUILDING FILE LISTS, output "%s" already exists.\n', output_mat_path);
+        end
     end
 end
